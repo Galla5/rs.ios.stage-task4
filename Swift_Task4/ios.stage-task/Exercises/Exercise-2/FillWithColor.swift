51927c50394833b40ae70acab1d00fb09bb4da7a
Poliakowa Galina <galapoliakowa2015@gmail.com>
2021-06-06 18:19:52 +0300

    feat: update task2 solution.

--- Swift_Task4/ios.stage-task/Exercises/Exercise-2/FillWithColor.swift
+++ Swift_Task4/ios.stage-task/Exercises/Exercise-2/FillWithColor.swift
@@ -4,41 +4,63 @@ final class FillWithColor {
     
     func fillWithColor(_ image: [[Int]], _ row: Int, _ column: Int, _ newColor: Int) -> [[Int]] {
         var resultImage = image
-        let currentPointColor = image[row][column]
-        
-        if row - 1 >= 0 {
-            let newPosition: (Int, Int) = (row - 1, column)
-            let topPonintColor = image[newPosition.0][newPosition.1]
-            if topPonintColor == currentPointColor {
-                resultImage[newPosition.0][newPosition.1] = newColor
-            }
+        let currectColor = image[row][column]
+        let allPoints = getAllPoints(image, row, column)
+        for point in allPoints {
+            resultImage = fillPointPixels(resultImage, point.0, point.1, newColor, currectColor)
         }
         
-        if row + 1 <= image.count - 1 {
-            let newPosition: (Int, Int) = (row + 1, column)
-            let bottomPonintColor = image[newPosition.0][newPosition.1]
-            if bottomPonintColor == currentPointColor {
-                resultImage[newPosition.0][newPosition.1] = newColor
-            }
+        return resultImage
+    }
+    
+    private func getAllPoints(_ image: [[Int]], _ row: Int, _ column: Int) -> [(Int, Int)] {
+        var allPoints = [(Int, Int)]()
+        let startPoint = (row, column)
+        allPoints.append(startPoint)
+        let firstDirectionalyPoints = getDirectionallyPoints(image, point: (row, column))
+        allPoints.append(contentsOf: firstDirectionalyPoints)
+        for dicPoint in firstDirectionalyPoints {
+            let secondPoints = getDirectionallyPoints(image, point: dicPoint)
+            allPoints.append(contentsOf: secondPoints)
+        }
+        return allPoints
+    }
+    
+    private func getDirectionallyPoints(_ image: [[Int]], point: (Int, Int)) -> [(Int, Int)] {
+        var points = [(Int, Int)]()
+        
+        if point.0 - 1 >= 0 {
+            points.append((point.0 - 1, point.1))
         }
         
-        if column - 1 >= 0 {
-            let newPosition: (Int, Int) = (row, column - 1)
-            let leftPonintColor = image[newPosition.0][newPosition.1]
-            if leftPonintColor == currentPointColor {
-                resultImage[newPosition.0][newPosition.1] = newColor
-            }
+        if point.0 + 1 <= image.count - 1 {
+            points.append((point.0 + 1, point.1))
         }
         
-        if column + 1 <= image[row].count - 1 {
-            let newPosition: (Int, Int) = (row, column + 1)
-            let rightPonintColor = image[newPosition.0][newPosition.1]
-            if rightPonintColor == currentPointColor {
-                resultImage[newPosition.0][newPosition.1] = newColor
-            }
+        if point.1 - 1 >= 0 {
+            points.append((point.0, point.1 - 1))
         }
         
-        resultImage[row][column] = newColor
+        if point.1 + 1 <= image[point.0].count - 1 {
+            points.append((point.0, point.1 + 1))
+        }
+        
+        return points
+    }
+    
+    private func fillPointPixels(
+        _ image: [[Int]],
+        _ row: Int,
+        _ column: Int,
+        _ newColor: Int,
+        _ currentPointColor: Int
+    ) -> [[Int]] {
+        var resultImage = image
+        let pointColor = resultImage[row][column]
+        if pointColor == currentPointColor {
+            resultImage[row][column] = newColor
+        }
         return resultImage
     }
 }
+
