4fc06b78f26bdf83c261099783344de5b9dd2945
Poliakowa Galina <galapoliakowa2015@gmail.com>
2021-06-06 19:04:20 +0300

    feat: add task3 solution.

--- Swift_Task4/ios.stage-task/Exercises/Exercise-3/CallStation.swift
+++ Swift_Task4/ios.stage-task/Exercises/Exercise-3/CallStation.swift
@@ -1,37 +1,68 @@
 import Foundation
 
-final class CallStation { }
+final class CallStation {
+    
+    fileprivate var allUsers = [User]()
+    fileprivate var allCalls = [Call]()
+}
 
 extension CallStation: Station {
     func users() -> [User] {
-        []
+        return allUsers
     }
     
     func add(user: User) {
-
+        guard !allUsers.contains(user) else { return }
+        allUsers.append(user)
     }
     
     func remove(user: User) {
-
+        guard let userIndex = allUsers.firstIndex(of: user) else { return }
+        allUsers.remove(at: userIndex)
     }
     
     func execute(action: CallAction) -> CallID? {
-        nil
+        switch action {
+        case .start(from: let user1, to: let user2):
+            let call = Call(id: UUID(), incomingUser: user1, outgoingUser: user2, status: .calling)
+            allCalls.append(call)
+            return call.id
+        case .answer(from: let user):
+            var currentCall = allCalls.first(where: { $0.incomingUser == user || $0.outgoingUser == user })
+            guard currentCall != nil else { return nil }
+            currentCall?.status = .talk
+            if let index = allCalls.firstIndex(where: { $0.id == currentCall?.id }) {
+                allCalls.remove(at: index)
+            }
+            allCalls.append(currentCall!)
+            return currentCall!.id
+        case .end(from: let user):
+            var currentCall = allCalls.first(where: { $0.incomingUser == user || $0.outgoingUser == user })
+            guard currentCall != nil else { return nil }
+            currentCall?.status = .ended(reason: .end)
+            if let index = allCalls.firstIndex(where: { $0.id == currentCall?.id }) {
+                allCalls.remove(at: index)
+            }
+            allCalls.append(currentCall!)
+            return currentCall!.id
+        default:
+            return nil
+        }
     }
     
     func calls() -> [Call] {
-        []
+        return allCalls
     }
     
     func calls(user: User) -> [Call] {
-        []
+        allCalls.filter { $0.incomingUser == user || $0.outgoingUser == user }
     }
     
     func call(id: CallID) -> Call? {
-        nil
+        return allCalls.first(where: { $0.id == id })
     }
     
     func currentCall(user: User) -> Call? {
-        nil
+        return allCalls.first(where: { $0.incomingUser == user || $0.outgoingUser == user })
     }
 }
